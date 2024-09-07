3f6ad001b2bd01b3705fcc8235ea0f01478d68e1
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-09-07 20:49:58 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
 		const [array, setArray] = useState(["You Need To Add New Task on The Task List"]);
 		const [inputValue, setInputValue] = useState('');
 		// Event handler for updating the array
-		const handleArrayUpdate = (evt) => {
+		const handleArrayADD = (evt) => {
 			if(evt.keyCode===13){
 				if (inputValue.trim() !== '') {
 					// Add a new element at the end of the array
@@ -19,10 +19,13 @@ const Home = () => {
 					setArray(newArray);
 		
 					// Clear the input field
-					setInputValue('');
+					setInputValue("");
 				}
 			}
 		};
+		const handleArrayUpdate=(evt)=>{
+			return(console.log(evt));
+		}
 		const handleArrayDelete=(evt)=>{
 			const newArray = [
 				...array.slice(0, parseInt(evt.target.id)), // Elements before the one to delete
@@ -49,21 +52,29 @@ const Home = () => {
 									value={inputValue}
 									onChange={(e) => setInputValue(e.target.value)}
 									placeholder="Enter a task"
-									onKeyDown={handleArrayUpdate}
+									onKeyDown={handleArrayADD}
 									
 								/>
 									</li>
 									{array.map((task,index)=>(
-									<div key={index}> <li  className={`list-group-item list-group-item-secondary ${index==0?"":"flex-container"}`} >
+									<div key={index}> <li  className={`list-group-item list-group-item-secondary ${index==0?"first":"flex-container"}`} >
 									<div className={index==0?"":"text"}>
-									{task} 	
+									{index==0?task:
+									<input type="text"
+									 defaultValue={task} 
+									 disabled
+									 />
+									} 	
 									</div>
-									<div className="img">
+									<div className="img flex-container">
 									<a onClick={handleArrayDelete} hidden={index==0?true:false}>
-									<svg id={index} xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#9e9e9e" fill="none" strokeLinecap="round" strokeLinejoin="round">
-									<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-									<path d="M18 6l-12 12" />
-									<path d="M6 6l12 12" />
+									<svg id={index} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+									<path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+									</svg>
+									</a>
+									<a onClick={handleArrayUpdate} hidden={index==0?true:false}>
+									<svg id={index} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+									<path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
 									</svg>
 									</a>
 									</div>
@@ -75,6 +86,8 @@ const Home = () => {
 									</li>
 								</ul>
 						</div>
+						<div className="pag1 list-group-item-secondary"></div>
+						<div className="pag2 list-group-item-secondary"></div>
 			</div>
 
 			
